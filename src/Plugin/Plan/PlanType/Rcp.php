0e28f548200e48a2722b0678399c143070ad99d2
Michael Stenta <mike@mstenta.net>
2025-05-27 16:11:40 -0400

    Add a farm organization reference field to RCPs.

--- src/Plugin/Plan/PlanType/Rcp.php
+++ src/Plugin/Plan/PlanType/Rcp.php
@@ -20,7 +20,22 @@ class Rcp extends FarmPlanType {
    * {@inheritdoc}
    */
   public function buildFieldDefinitions() {
-    return [];
+    $fields = [];
+    $field_info = [
+      'farm' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Farm'),
+        'description' => $this->t('Associates the resource conservation plan with a farm organization.'),
+        'target_type' => 'organization',
+        'target_bundle' => 'farm',
+        'cardinality' => 1,
+        'required' => TRUE,
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = \Drupal::service('farm_field.factory')->bundleFieldDefinition($info);
+    }
+    return $fields;
   }
 
 }
