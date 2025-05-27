c4b1ab97e170472704f292ea4523645fa448a03b
Michael Stenta <mike@mstenta.net>
2025-05-27 10:39:46 -0400

    Rename to farm_rcp: Resource Conservation Plan.

---
+++ src/Plugin/Plan/PlanType/Rcp.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\farm_rcp\Plugin\Plan\PlanType;
+
+use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
+
+/**
+ * Provides the resource conservation plan type.
+ *
+ * @PlanType(
+ *   id = "rcp",
+ *   label = @Translation("Resource conservation"),
+ * )
+ */
+class Rcp extends FarmPlanType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    return [];
+  }
+
+}
