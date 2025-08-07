bf30cafb655c47051f14094cbd4f7e895f62404c
Michael Stenta <mike@mstenta.net>
2025-08-07 07:58:47 -0400

    Set log status to pending and add review flag.

--- src/Plugin/QuickForm/Intake.php
+++ src/Plugin/QuickForm/Intake.php
@@ -853,6 +853,8 @@ class Intake extends QuickFormBase {
       'intake_interests' => $intake_interests,
       'intake_interests_comments' => $saved_values['interests']['interests']['comments'],
       'intake_rcd_sharing_allowed' => $intake_rcd_sharing_allowed,
+      'status' => 'pending',
+      'flag' => ['review'],
     ]);
   }
 
