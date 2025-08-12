01932f28c45ada0904c529d60e0737248932bc80
Michael Stenta <mike@mstenta.net>
2025-08-12 12:20:49 -0400

    Show all active manager users in dropdown.

--- src/Form/IntakeReviewForm.php
+++ src/Form/IntakeReviewForm.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Drupal\farm_sli\Form;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\log\Entity\LogInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Form that renders quick forms.
@@ -16,6 +18,32 @@ use Drupal\log\Entity\LogInterface;
  * @ingroup farm
  */
 class IntakeReviewForm extends FormBase {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Class constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The quick form instance manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -56,12 +84,24 @@ class IntakeReviewForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    // Build a list of active managers.
+    $users = $this->entityTypeManager->getStorage('user')->loadByProperties([
+      'roles' => 'farm_manager',
+      'status' => TRUE,
+    ]);
+    $owner_options = array_combine(
+      array_keys($users),
+      array_map(function ($user) {
+        return $user->label();
+      }, $users),
+    );
+
     // Assign intake owner dropdown.
     $form['owner'] = [
       '#type' => 'select',
       '#title' => $this->t('Assign ownership'),
       '#description' => $this->t('Who will be responsible for this intake?'),
-      '#options' => [],
+      '#options' => $owner_options,
       '#required' => TRUE,
     ];
 
