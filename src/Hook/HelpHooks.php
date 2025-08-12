50fcf3fa0da44fce4161e404a2894c9298131c5b
Michael Stenta <mike@mstenta.net>
2025-08-12 11:59:08 -0400

    Add help text to intake review form.

---
+++ src/Hook/HelpHooks.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\farm_sli\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Help hook implementations for farm_sli.
+ */
+class HelpHooks {
+
+  use StringTranslationTrait;
+
+  /**
+   * Implements hook_help().
+   */
+  #[Hook('help')]
+  public function help(string $route_name, RouteMatchInterface $route_match): string|\Stringable|array|null  {
+
+    // Intake review form.
+    if ($route_name == 'farm_sli.intake_review') {
+      return $this->t('Use this form to review an intake, assign ownership, and decide whether to continue, postpone, or abandon.');
+    }
+
+    return NULL;
+  }
+
+}
