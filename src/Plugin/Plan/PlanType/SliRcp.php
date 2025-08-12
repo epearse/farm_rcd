1a9b7ed7cd1ef82ea55b928624438654294129cb
Michael Stenta <mike@mstenta.net>
2025-08-12 13:44:41 -0400

    Rename rcp to sli_rcp and "profile" back to "plan".

--- src/Plugin/Plan/PlanType/SliRcp.php
+++ src/Plugin/Plan/PlanType/SliRcp.php
@@ -9,13 +9,13 @@ use Drupal\farm_entity\Attribute\PlanType;
 use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
 
 /**
- * Provides the resource conservation profile plan type.
+ * Provides the SLI resource conservation plan type.
  */
 #[PlanType(
-  id: 'rcp',
-  label: new TranslatableMarkup('Resource conservation profile')),
-]
-class Rcp extends FarmPlanType {
+  id: 'sli_rcp',
+  label: new TranslatableMarkup('Resource conservation'),
+)]
+class SliRcp extends FarmPlanType {
 
   /**
    * {@inheritdoc}
@@ -28,7 +28,7 @@ class Rcp extends FarmPlanType {
       'farm' => [
         'type' => 'entity_reference',
         'label' => $this->t('Farm'),
-        'description' => $this->t('Associates the resource conservation profile with a farm organization.'),
+        'description' => $this->t('Associates the resource conservation plan with a farm organization.'),
         'target_type' => 'organization',
         'target_bundle' => 'farm',
         'cardinality' => 1,
